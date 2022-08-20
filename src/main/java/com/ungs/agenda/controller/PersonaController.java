7b4a38fe47117a587b830b3808f6cf778136c026
Mariano Franzetti <marianofranzetti@outlook.com.com>
2022-08-20 00:48:08 -0300

    se suben clases html que dan funcionamiento a la agenda y se sube el
    controlador de la persona que maneja los metodos del crud

--- src/main/java/com/ungs/agenda/controller/PersonaController.java
+++ src/main/java/com/ungs/agenda/controller/PersonaController.java
@@ -1,14 +1,49 @@
 package com.ungs.agenda.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ungs.agenda.model.Persona;
 import com.ungs.agenda.repository.PersonaRepository;
 
 @Controller
+@RequestMapping("/")
 public class PersonaController {
 	@Autowired
 	private PersonaRepository personaRepository;
 	
 	
+	@RequestMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("list", personaRepository.findAll());
+		return "index";
+	}
+	
+	
+	@RequestMapping("/editar")
+	public String editar(Model model) {
+		
+		model.addAttribute("list", personaRepository.findAll());
+		return "editar";
+	}
+	
+	@RequestMapping("/eliminar/{id}")
+	public String eliminar(Model model) {
+		
+		//model.addAttribute("list", personaRepository.findAll());
+		return "eliminar";
+	}
+	
+	@RequestMapping("/eliminar/confirmacion/{id}")
+	public String confirmacionEliminar(Model model) {
+		
+		//personaRepository.deleteById(model.)
+		return "index";
+	}
 }
