011179bd2104a051ec2614a4f470b1d03c27c1b2
cris-esm <cris.sm.1998@gmail.com>
2022-08-20 18:51:54 -0300

    Se agrego tipoContacto al modelo

--- src/main/java/com/ungs/agenda/model/TipoContacto.java
+++ src/main/java/com/ungs/agenda/model/TipoContacto.java
@@ -1,8 +1,34 @@
 package com.ungs.agenda.model;
 
+import java.util.List;
 
-public enum TipoContacto {
-	TRABAJO,
-	FAMILIA,
-	AMIGOS
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@Entity
+@Table(name = "tipocontacto")
+public class TipoContacto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nombre;
+	
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	@OneToMany(mappedBy = "tipoContacto", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Persona> personas;
 }
